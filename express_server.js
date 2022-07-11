6971c037424b1e5a18f56e2e2f32ca4b0a151ff4
MT91-dev <tariqm1991@gmail.com>
2022-07-11 19:22:37 +0000

    Added url encoding format, get for urls new, post for urls, and function to generate random string of 6 alphanumeric values

--- express_server.js
+++ express_server.js
@@ -5,10 +5,18 @@ const PORT = 8080; // default port 8080
 //setting the view engine as ejs
 app.set("view engine", "ejs");
 
+//generating a random unique short url id
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,6 +31,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
   const templateVars = { id, longURL: urlDatabase[id] };
@@ -31,6 +43,12 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+//Post request logic that handles how to process the users input that is submittted
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
