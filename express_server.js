7625c156f0216eb1a8fffa1f76fac72f641bbc02
MT91-dev <tariqm1991@gmail.com>
2022-07-14 23:26:18 +0000

    Required helper functions, updated logic as per hashed cookies

--- express_server.js
+++ express_server.js
@@ -1,66 +1,25 @@
+//Requiring libraries/frameworks
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const bcrypt = require("bcryptjs");
+const cookieSession = require('cookie-session');
+
+//settings
 const salt = bcrypt.genSaltSync(10);
 const app = express();
 const PORT = 8080; // default port 8080
-
-//settings
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["cant touch this", "only if you are Neo from Matrix"],
 
-//generating a random unique short url id
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
-function generateRandomString() {
-  return Math.random().toString(36).substr(2, 6);
-}
-
-//function to check if cookie with user_id exists
-const validateCookie = (cookie_id) => {
-  for (let user in users) {
-    if (users[user].id === cookie_id) {
-      return users[user];
-    };
-  };
-  return undefined;
-}
-
-//function to check if user exists in user object with email entered
-const getUserByEmail = (email) => {
-  for (const user in users) {
-    if (users[user].email === email) {
-      return users[user];
-    };
-  };
-  return undefined;
-}
-
-//function to check if password is correct based on users object for user logging in
-const validPassword = (password) => {
-  for (const user in users) {
-    if (users[user].password === password) {
-      return true;
-    };
-  };
-  return false;
-}
-
-//function to get all URLs by user_id
-const getAllUrlsByUser = (cookie) => {
-  let currentUserObject = {};
-  let currentUser = cookie;
-  for (const url in nestedURLDatabase) {
-    if (nestedURLDatabase[url].userID === currentUser) {
-      let key = url;
-      let value = nestedURLDatabase[url]["longURL"];
-      currentUserObject[key] = value;
-    };
-  };
-  return currentUserObject;
-}
-
-//Users database object
+//Users database object which is updated when new user is created
 const users = {
   example: {
     id: "b2xVn2",
@@ -69,12 +28,13 @@ const users = {
   },
 };
 
-//URLs database
+//URLs database //not used anymore since we have nested object for database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Nested URL database with unique id's for keys that hold url information that are generated each time a user generates a new url
 const nestedURLDatabase = {
   example: {
     userID: "b2xVn2",
@@ -86,24 +46,27 @@ const nestedURLDatabase = {
   },
 };
 
+//requiring helper functions
+const { generateRandomString, validateCookie, getUserByEmail, validPassword, getAllUrlsByUser } = require('./helpers');
+
 app.get("/", (req, res) => {
-  let currentObject = getAllUrlsByUser(req.cookies["user_id"]);
+  let currentObject = getAllUrlsByUser(req.session.user_id, nestedURLDatabase);
   const templateVars = {
     urls: currentObject,
-    user: validateCookie(req.cookies["user_id"])
+    user: validateCookie(req.session.user_id, users)
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
+  const loginState = validateCookie(req.session.user_id, users);
   if (!loginState) {
-    res.send("Please login to use TinyApp")
+    res.send("Please login to use TinyApp");
   } else {
-    let currentObject = getAllUrlsByUser(req.cookies["user_id"]);
+    let currentObject = getAllUrlsByUser(req.session.user_id, nestedURLDatabase);
     const templateVars = {
       urls: currentObject,
-      user: validateCookie(req.cookies["user_id"])
+      user: validateCookie(req.session.user_id, users)
     };
     // console.log("templateVars", templateVars);
     res.render("urls_index", templateVars);
@@ -111,12 +74,12 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
+  const loginState = validateCookie(req.session.user_id, users);
   if (!loginState) {
     res.redirect(`/login`);
   } else {
     const templateVars = {
-      user: validateCookie(req.cookies["user_id"])
+      user: validateCookie(req.session.user_id, users)
       // username: req.cookies["username"],
     };
     res.render("urls_new", templateVars);
@@ -124,50 +87,52 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
-  const currentUserObject = getAllUrlsByUser(req.cookies["user_id"]);
-  if (!loginState){
-    res.send("Please login to use TinyApp")
+  const loginState = validateCookie(req.session.user_id, users);
+  const currentUserObject = getAllUrlsByUser(req.session.user_id, nestedURLDatabase);
+  // console.log(nestedURLDatabase);
+  // console.log(currentUserObject);
+  if (!loginState) {
+    res.send("Please login to use TinyApp");
   } else if (currentUserObject[req.params.id] === undefined) {
-    res.send("You do not have permissions to view or change this URL")
+    res.send("You do not have permissions to view or change this URL");
   } else {
     const templateVars = {
       id: req.params.id,
       longURL: currentUserObject[req.params.id],
-      user: validateCookie(req.cookies["user_id"])
+      user: validateCookie(req.session.user_id, users)
     };
     res.render("urls_show", templateVars); //200 level status code
   }
 });
 
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id
-  res.redirect(urlDatabase[id]); //300 level status code
+  const id = req.params.id;
+  res.redirect(nestedURLDatabase[id]); //300 level status code
 });
 
 app.get("/register", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
+  const loginState = validateCookie(req.session.user_id, users);
   if (loginState) {
     res.redirect(`/urls`);
   } else {
     const templateVars = {
       id: req.params.id,
-      longURL: urlDatabase[req.params.id],
-      user: validateCookie(req.cookies["user_id"])
+      longURL: nestedURLDatabase[req.params.id],
+      user: validateCookie(req.session.user_id, users)
     };
     res.render("urls_registration", templateVars);
   }
 });
 
 app.get("/login", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
+  const loginState = validateCookie(req.session.user_id, users);
   if (loginState) {
     res.redirect(`/urls`);
   } else {
     const templateVars = {
       id: req.params.id,
-      longURL: urlDatabase[req.params.id],
-      user: validateCookie(req.cookies["user_id"])
+      longURL: nestedURLDatabase[req.params.id],
+      user: validateCookie(req.session.user_id, users)
     };
     res.render("urls_login", templateVars);
   }
@@ -175,77 +140,74 @@ app.get("/login", (req, res) => {
 
 //Post request logic that handles how to process the users input that is submittted
 app.post("/urls", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
+  const loginState = validateCookie(req.session.user_id, users);
   if (!loginState) {
     res.send("You are unable to shorten the URL because you are either not logged in or are not a registered user.");
   } else {
     const tempId = generateRandomString();
-    urlDatabase[tempId] = req.body.longURL;
-    // console.log(urlDatabase);
-    // console.log(req.body); // Log the POST request body to the console
-    // console.log(req.body.longURL); // Log the POST request body to the console
-    // console.log(urlDatabase, "Did it work?");
+    nestedURLDatabase[tempId] = {
+      userID: req.session.user_id,
+      longURL: req.body.longURL,
+    };
     res.redirect(`/urls/${tempId}`); // Respond with 'Ok' (we will replace this)
   }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const loginState = validateCookie(req.cookies["user_id"]);
-  const currentUserObject = getAllUrlsByUser(req.cookies["user_id"]);
-  if (!loginState){
-    res.send("Please login to use TinyApp")
+  const loginState = validateCookie(req.session.user_id, users);
+  const currentUserObject = getAllUrlsByUser(req.session.user_id, nestedURLDatabase);
+  if (!loginState) {
+    res.send("Please login to use TinyApp");
   } else if (currentUserObject[req.params.id] === undefined) {
-    res.send("You do not have permissions to view or change this URL")
+    res.send("You do not have permissions to view or change this URL");
   } else {
-    delete urlDatabase[req.params.id];
-    // const templateVars = { urls: urlDatabase }
+    delete nestedURLDatabase[req.params.id];
     res.redirect(`/urls`);
-    // res.render(("urls_index", templateVars)); // Respond with 'Ok' (we will replace this)
   }
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  // console.log(req.body.longURL);
-  // console.log(urlDatabase);
-  // const templateVars = { urls: urlDatabase }
+  nestedURLDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect(`/urls`);
-  // res.render(("urls_index", templateVars)); // Respond with 'Ok' (we will replace this)
+  // res.render(("urls_index", templateVars));
 });
 
 app.post("/login", (req, res) => {
-  if (!getUserByEmail(req.body.email.trim())) {
-    res.status(403).send("This user does not exist.")
-  } else if (!bcrypt.compareSync(req.body.password, getUserByEmail(req.body.email).password)) {
-    res.status(403).send("The password you have entered is incorrect.")
+  if (!getUserByEmail(req.body.email.trim(), users)) {
+    res.status(403).send("This user does not exist.");
+  } else if (!bcrypt.compareSync(req.body.password, getUserByEmail(req.body.email.trim(), users).password)) {
+    res.status(403).send("The password you have entered is incorrect.");
   } else {
-    res.cookie("user_id", getUserByEmail(req.body.email).id);
+    req.session.user_id = getUserByEmail(req.body.email, users).id;
+    // res.cookie("user_id", getUserByEmail(req.body.email).id);
     res.redirect(`/urls`);
   }
-  console.log(users);
+  // console.log(users);
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
-  res.redirect("/login")
+  req.session = null;
+  // res.clearCookie("user_id");
+  res.redirect("/login");
 });
 
 app.post("/register", (req, res) => {
   const userId = generateRandomString();
   if (!req.body.email.trim() || !req.body.password.trim()) {
-    res.status(400).send("Please enter a valid email and password.")
+    res.status(400).send("Please enter a valid email and password.");
   } else if (getUserByEmail(req.body.email)) {
-    res.status(400).send("Sorry, but that email already exists!")
+    res.status(400).send("Sorry, but that email already exists!");
   } else {
-    res.cookie("user_id", userId);
+    req.session.user_id = userId;
+    // res.cookie("user_id", userId);
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
     users[userId] = {
       id: userId,
       email: req.body.email,
       password: hashedPassword,
-    }
-  };
-  console.log(users);
+    };
+  }
+  // console.log(users);
   res.redirect(`/urls`);
 });
 
